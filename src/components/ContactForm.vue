b266c3d369571e4de513da6052832d189afa38d9
Taimai14 <maiduytai2002@gmail.com>
2024-03-28 15:12:51 +0700

    Bai tap tu lam

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -34,38 +34,29 @@
         </div>
     </Form>
 </template>
-
 <script>
-import * as yup from "yup";
-import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from 'yup';
+import { Form, Field, ErrorMessage } from 'vee-validate';
 export default {
     components: {
         Form,
         Field,
         ErrorMessage,
     },
-    emits: ["submit:contact", "delete:contact"],
+    emits: ['submit:contact', 'delete:contact'],
     props: {
-        contact: { type: Object, required: true }
+        contact: { type: Object, required: true },
     },
     data() {
         const contactFormSchema = yup.object().shape({
             name: yup
                 .string()
-                .required("Tên phải có giá trị.")
-                .min(2, "Tên phải ít nhất 2 ký tự.")
-                .max(50, "Tên có nhiều nhất 50 ký tự."),
-            email: yup
-                .string()
-                .email("E-mail không đúng.")
-                .max(50, "E-mail tối đa 50 ký tự."),
-            address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
-            phone: yup
-                .string()
-                .matches(
-                    /((09|03|07|08|05)+([0-9]{8})\b)/g,
-                    "Số điện thoại không hợp lệ."
-                ),
+                .required('Tên phải có giá trị.')
+                .min(2, 'Tên phải ít nhất 2 ký tự.')
+                .max(50, 'Tên có nhiều nhất 50 ký tự.'),
+            email: yup.string().email('E-mail không đúng.').max(50, 'E-mail tối đa 50 ký tự.'),
+            address: yup.string().max(100, 'Địa chỉ tối đa 100 ký tự.'),
+            phone: yup.string().matches(/((09|03|07|08|05)+([0-9]{8})\b)/g, 'Số điện thoại không hợp lệ.'),
         });
         return {
             contactLocal: this.contact,
@@ -74,15 +65,14 @@ export default {
     },
     methods: {
         submitContact() {
-            this.$emit("submit:contact", this.contactLocal);
+            this.$emit('submit:contact', this.contactLocal);
         },
         deleteContact() {
-            this.$emit("delete:contact", this.contactLocal.id);
+            this.$emit('delete:contact', this.contactLocal.id);
         },
     },
 };
 </script>
-
 <style scoped>
-@import "@/assets/form.css";
+@import '@/assets/form.css';
 </style>
